f514a07460ae511ff7342e973de4cc8f529d44c3
elijah <tomelijah349@gmail.com>
2025-07-25 01:50:39 +0100

    make nav bar more responsive

--- script.js
+++ script.js
@@ -11,3 +11,11 @@ btn.addEventListener("click", () => {
     icon.classList.replace("fa-times", "fa-bars");
   }
 });
+
+// Initialize AOS (Animate On Scroll)
+AOS.init({
+  duration: 1000, // Animation duration (1s)
+  easing: "ease-in-out", // Smooth easing
+  once: false, // Animate everytime
+  mirror: true,
+});
